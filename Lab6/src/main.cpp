384fc8620542fc3c34fb1257b0db15ae8bb457e5
Melmaphother <1559792558@qq.com>
2023-11-15 23:34:55 +0800

    modify cmd parameters

--- Lab6/src/main.cpp
+++ Lab6/src/main.cpp
@@ -3,37 +3,33 @@
 #include <cassert>
 #include <string.h>
 #include <string>
+#include <utility>
+
+std::pair<bool, std::string> getCmdOption(char **begin, char **end,
+										  const std::string &option) {
+	char **itr = std::find(begin, end, option);
+	if (itr != end && ++itr != end) { return std::make_pair(true, *itr); }
+	return std::make_pair(false, "");
+}
 
 bool cmdOptionExists(char **begin, char **end, const std::string &option) {
 	return std::find(begin, end, option) != end;
 }
 
 int main(int argc, char **argv) {
-	std::string data_path = "..\\data\\original.txt";
-	std::string store_path = "..\\data\\table.txt";
-	if (cmdOptionExists(argv, argv + argc, "-h")) {
+	std::string data_path	= "..\\data\\original.txt";
+	std::string store_path	= "..\\data\\table.txt";
+	auto		help_info	= cmdOptionExists(argv, argv + argc, "-h");
+	auto		input_info	= getCmdOption(argv, argv + argc, "-f");
+	auto		output_info = getCmdOption(argv, argv + argc, "-o");
+	if (help_info) {
 		std::cout << "-h : help message" << std::endl;
 		std::cout << "-f [file path] : input file path" << std::endl;
 		std::cout << "-o [file path] : output file path" << std::endl;
 	}
-	if (cmdOptionExists(argv, argv + argc, "-f")) {
-		if (argc >= 3) {
-			std::cout << "argv2: " << argv[2] << ' ' << strlen(argv[2])
-					  << std::endl;
-			data_path = argv[2];
-		} else {
-			std::cout << "Lack of parameter." << std::endl;
-			std::cout << "Use \"-h\" to get some help." << std::endl;
-		}
-	}
-	if (cmdOptionExists(argv, argv + argc, "-o")) {
-		if (argc >= 3) {
-			store_path = argv[2];
-		} else {
-			std::cout << "Lack of parameter." << std::endl;
-			std::cout << "Use \"-h\" to get some help." << std::endl;
-		}
-	}
+	if (input_info.first) data_path = input_info.second;
+	if (output_info.first) store_path = output_info.second;
+    
 	HuffmanCode huffman(data_path, store_path);
 	huffman.CalcCompressRatio();
 	return 0;
